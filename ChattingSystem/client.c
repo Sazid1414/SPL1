4cf090849bbcf77d5df68e32809f1254020ed925
Sazid1414 <bsse1414@iit.du.ac.bd>
2023-11-06 11:00:11 -0500

    New code

--- ChattingSystem/client.c
+++ ChattingSystem/client.c
@@ -15,123 +15,135 @@
 #define NAME_LEN 32
 #define LENGTH 2048
 
-volatile sig_atomic_t flag=0;
-int sockfd=0;
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
 char name[NAME_LEN];
 
 void str_overwrite_stdout()
 {
-    printf("\r%s", "> ");
-    fflush(stdout);
+  printf("\r%s", "> ");
+  fflush(stdout);
 }
 
 void str_trim_lf(char *arr, int length)
 {
-    for (int i = 0; i < length; i++)
+  for (int i = 0; i < length; i++)
+  {
+    if (arr[i] == '\n')
     {
-        if (arr[i] == '\n')
-        {
-            arr[i] = '\0';
-            break;
-        }
+      arr[i] = '\0';
+      break;
     }
-}
-void recv_msg_handler() {
-	char message[LENGTH] = {};
-  while (1) {
-		int receive = recv(sockfd, message, LENGTH, 0);
-    if (receive > 0) {
-      printf("%s", message);
-      str_overwrite_stdout();
-    } else if (receive == 0) {
-			break;
-    } else {
-			// -1
-		}
-		memset(message, 0, sizeof(message));
   }
 }
-void send_msg_handler() {
-  char message[LENGTH] = {};
-	char buffer[LENGTH + 32] = {};
-
-  while(1) {
-  	str_overwrite_stdout();
-    fgets(message, LENGTH, stdin);
-    str_trim_lf(message, LENGTH);
-
-    if (strcmp(message, "exit") == 0) {
-			break;
-    } else {
-      sprintf(buffer, "%s: %s\n", name, message);
-      send(sockfd, buffer, strlen(buffer), 0);
+void catch_ctrl_c_and_exit(int sig)
+{
+  flag = 1;
+}
+void recv_msg_handler()
+{
+  char message[BUFFER_SIZE] = {};//buffer==length
+  while (1)
+  {
+    int receive = recv(sockfd, message,LENGTH, 0);//
+    if (receive > 0)
+    {
+      printf("%s", message);
+      str_overwrite_stdout();
     }
-
-		bzero(message, LENGTH);
-    bzero(buffer, LENGTH + 32);
+    else if (receive == 0)
+    {
+      break;
+    }
+    // else
+    // {
+    //   // -1
+    // }
+    // memset(message, 0, sizeof(message));
+    bzero(message,BUFFER_SIZE);
+  }
+}
+void send_msg_handler()
+{
+  char buffer[BUFFER_SIZE] = {};
+  char message[BUFFER_SIZE+NAME_LEN] = {};
+  while(1)
+  {
+    str_overwrite_stdout();
+    fgets(buffer,BUFFER_SIZE,stdin);
+    str_trim_lfirst(buffer,BUFFER_SIZE);
+    if(strcmp(buffer,"exit") == 0)
+    {
+      break;
+    }
+    else{
+      sprintf(message,"%s: %s",name,message);
+      send(sockfd,message,strlen(message),0);
+    }
+    bzero(buffer,BUFFER_SIZE);
+    bzero(message,BUFFER_SIZE+NAME_LEN);
   }
   catch_ctrl_c_and_exit(2);
 }
-void catch_ctrl_c_and_exit(int sig) 
-{
-    flag = 1;
-}
-
 int main(int argc, char **argv)
 {
-    if(argc!= 2)
-    {
-        printf("Usage:%s<port>\n",argv[0]);
-        return EXIT_FAILURE;
-    }
-    char *ip="127.0.0.1";
-    int port=atoi(argv[1]);
-
-    signal(SIGINT,catch_ctrl_c_and_exit);
-    printf("Enter Your Name: ");
-    fgets(name,NAME_LEN,stdin);
-    str_trim_lf(name,strlen(name));
-    if(strlen(name)>NAME_LEN-1||strlen(name)<2)
-    {
-        printf("Enter name Correctly");
-        return EXIT_FAILURE;
-    }
-    struct sockaddr_in server_addr;
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    server_addr.sin_port = htons(port);
-
-    int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if(err==-1)
-    {
-        printf("Error connecting\n");
-        return EXIT_FAILURE;
-    }
-    send(sockfd,name,NAME_LEN,0);
-
-    printf("****WELCOME TO CHAT ROOM****\n");
-
-    	pthread_t send_msg_thread;
-  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
-		printf("ERROR: pthread\n");
+  if (argc != 2)
+  {
+    printf("Usage:%s<port>\n", argv[0]);
     return EXIT_FAILURE;
-	}
+  }
+  char *ip = "127.0.0.1";
+  int port = atoi(argv[1]);
 
-	pthread_t recv_msg_thread;
-  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
-		printf("ERROR: pthread\n");
-		return EXIT_FAILURE;
-	}
+  signal(SIGINT, catch_ctrl_c_and_exit);
+  printf("Enter Your Name: ");
+  fgets(name, NAME_LEN, stdin);
+  str_trim_lf(name, strlen(name));
+  if (strlen(name) > NAME_LEN - 1 || strlen(name) < 2)
+  {
+    printf("Enter name Correctly");
+    return EXIT_FAILURE;
+  }
+  struct sockaddr_in server_addr;
 
-	while (1){
-		if(flag){
-			printf("\nBye\n");
-			break;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+  server_addr.sin_port = htons(port);
+
+  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err == -1)
+  {
+    printf("Error connecting\n");
+    return EXIT_FAILURE;
+  }
+  send(sockfd, name, NAME_LEN, 0);
+
+  printf("****WELCOME TO CHAT ROOM****\n");
+
+  pthread_t send_msg_thread;
+  if (pthread_create(&send_msg_thread, NULL, (void *)send_msg_handler, NULL) != 0)
+  {
+    printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+  }
+
+  pthread_t recv_msg_thread;
+  if (pthread_create(&recv_msg_thread, NULL, (void *)recv_msg_handler, NULL) != 0)
+  {
+    printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+  }
+
+  while (1)
+  {
+    if (flag)
+    {
+      printf("\nBye\n");
+      break;
     }
-	}
+  }
 
-	close(sockfd);
-    return EXIT_SUCCESS;
+  close(sockfd);
+  return EXIT_SUCCESS;
 }
